409a54fde43f90c2050b0ea8880c7fcf30875760
abkazybek <kazbek529@gmail.com>
2021-11-15 01:25:13 +0600

    Initial commit

---
+++ config-service/src/main/java/com/javastart/config/ConfigApplication.java
@@ -0,0 +1,16 @@
+package com.javastart.config;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+
+    }
+}
